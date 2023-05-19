bff739fccad9c0c44b78b0733c89fcbaa24e86e2
Ernest Badu <ernestjbadu@outlook.com>
2023-05-19 15:25:49 +0100

    chore: code quality improvements

--- packages/theme-docs/src/layouts/index.d.ts
+++ packages/theme-docs/src/layouts/index.d.ts
@@ -1,3 +1,25 @@
+import type { MarkdownHeading, MarkdownInstance } from "astro";
+
 export type { default as DocsLayout } from "./DocsLayout.astro";
 export type { default as PageLayout } from "./PageLayout.astro";
 export type { default as PulsarPage } from "./PulsarPage.astro";
+
+/**
+ * Duplicated from 'astro:content' as Pulsar cannot import
+ * generated files from consuming projects.
+ */
+export interface PulsarCollectionEntry<Schema extends object> {
+  id: string;
+  slug: string;
+  body: string;
+  collection: string;
+  data: Schema;
+  render(): Render[".md"];
+}
+
+interface Render {
+  ".md": Promise<{
+    Content: MarkdownInstance["Content"];
+    headings: MarkdownHeading[];
+  }>;
+}
