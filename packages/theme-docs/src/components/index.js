1bcbfec5c1954731eb26886c66ae705a63c5c193
Ernest Badu <ernestjbadu@outlook.com>
2023-05-20 15:34:57 +0100

    chore: update component exports

--- packages/theme-docs/src/components/index.js
+++ packages/theme-docs/src/components/index.js
@@ -1,1 +1,3 @@
-export { default as Code } from "./ui/Code.astro";
+export { default as Select } from "./ui/Select.astro";
+export { default as ThemeSwitcher } from "./ui/ThemeSwitcher.astro";
+export { default as AccordionItem } from "./ui/Accordion/AccordionItem.astro";
