c534ea6cdccacfd928bfce71d8517094392ec542
Ernest Badu <ernestjbadu@outlook.com>
2023-05-21 19:13:09 +0100

    chore: add tests for building github edit url

---
+++ packages/theme-docs/src/layouts/PulsarPage/__test__/useEditUrl.test.js
@@ -0,0 +1,124 @@
+import { describe, expect, test } from "vitest";
+import { useEditUrl } from "../lib";
+
+describe("useEditUrl", () => {
+  describe("With editInGitHub set to false", () => {
+    test("should return null", () => {
+      const editUrl = useEditUrl({
+        siteConfig: {
+          repositories: {
+            project: { url: "some-url" },
+          },
+        },
+        pageCollection: {
+          collection: "pages",
+          id: "some-id",
+          data: { type: "docs", editInGitHub: false },
+        },
+      });
+
+      expect(editUrl).toBeNull();
+    });
+  });
+
+  describe("With no project or documentation repository", () => {
+    test("should return null", () => {
+      const editUrl = useEditUrl({
+        siteConfig: {
+          repositories: {},
+        },
+        pageCollection: {
+          collection: "pages",
+          id: "some-id",
+          data: { type: "docs", editInGitHub: true },
+        },
+      });
+
+      expect(editUrl).toBeNull();
+    });
+  });
+
+  describe("With a collection entry used in a page subdirectory of the same name", () => {
+    test("should return the correct github url", () => {
+      const editUrl = useEditUrl({
+        siteConfig: {
+          repositories: {
+            project: { url: "https://github.com/owner/repo" },
+          },
+        },
+        pageCollection: {
+          collection: "docs",
+          id: "en/overview/core-concepts.md",
+          data: { type: "docs", editInGitHub: true },
+        },
+      });
+
+      expect(editUrl).toBe(
+        "https://github.com/owner/repo/src/content/docs/en/overview/core-concepts.md"
+      );
+    });
+  });
+
+  describe("With a collection entry used in a page subdirectory of a different name", () => {
+    test("should return the correct github url", () => {
+      const editUrl = useEditUrl({
+        siteConfig: {
+          repositories: {
+            project: { url: "https://github.com/owner/repo" },
+          },
+        },
+        pageCollection: {
+          collection: "docs",
+          id: "en/overview/core-concepts.md",
+          data: { type: "docs", editInGitHub: true },
+        },
+      });
+
+      expect(editUrl).toBe(
+        "https://github.com/owner/repo/src/content/docs/en/overview/core-concepts.md"
+      );
+    });
+  });
+
+  describe("With a collection entry used at the root of the site", () => {
+    test("should return the correct github url", () => {
+      const editUrl = useEditUrl({
+        siteConfig: {
+          repositories: {
+            project: { url: "https://github.com/owner/repo" },
+          },
+        },
+        pageCollection: {
+          collection: "docs",
+          id: "en/overview/core-concepts.md",
+          data: { type: "docs", editInGitHub: true },
+        },
+      });
+
+      expect(editUrl).toBe(
+        "https://github.com/owner/repo/src/content/docs/en/overview/core-concepts.md"
+      );
+    });
+  });
+
+  describe("With a custom docs repository", () => {
+    test("should return the correct github url", () => {
+      const editUrl = useEditUrl({
+        siteConfig: {
+          repositories: {
+            documentation: { url: "https://github.com/owner/repo" },
+          },
+        },
+        pageCollection: {
+          collection: "docs",
+          id: "en/overview/core-concepts.md",
+          data: { type: "docs", editInGitHub: true },
+        },
+      });
+
+      expect(editUrl).toBe(
+        "https://github.com/owner/repo/src/content/docs/en/overview/core-concepts.md"
+      );
+    });
+  });
+});
